06e3d3b324673c37231c55c08fa9cd057c7c4796
raph-abder <abdraphael@gmail.com>
2025-08-15 10:16:04 -0400

    migrated the contract again

--- client/src/constants/contracts.js
+++ client/src/constants/contracts.js
@@ -1,3 +1,3 @@
 export const CONTRACT_ADDRESSES = {
-  DID_REGISTRY: "0x047E927ACe38e9ca160e5B531C1A18643E1fBfe1", // Change this file for the correct address in your Ganache setup
+  DID_REGISTRY: "0xB1EC5280B529D5eFa29D7a9d73c493E376320E69", // Change this file for the correct address in your Ganache setup
 };
